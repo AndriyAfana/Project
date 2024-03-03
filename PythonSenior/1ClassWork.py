999f64c69530000e18eadf1f7022764ba14ffb3d
Andriy <andriyafanasenko66@gmail.com>
2024-03-03 14:09:56 +0200

    V.7

--- PythonSenior/1ClassWork.py
+++ PythonSenior/1ClassWork.py
@@ -1,3 +1,4 @@
 print("Python")
 a = input("What your name: ")
-print(a + ", welcome to new lesson" - "Python Senior")
+print(a + ", welcome to new lesson" + " - Python Senior")
+b = input("Do you complete lessons: Python Kids and Python Junior. Yes or No: ")
