f0d29e0bc9e693f8853c89d5670737d4b81702a1
Andriy <andriyafanasenko66@gmail.com>
2024-03-03 14:21:13 +0200

    V.1

---
+++ PythonSenior/2ClassWork.py
@@ -0,0 +1,8 @@
+a = int(input("Введіть перше число: "))
+b = int(input("Введіть друге число: "))
+c = int(input("Введіть третє число: "))
+
+print(f'{a} + {b} + {c} = {a + b + c}')
+print(f'{a} - {b} - {c} = {a - b - c}')
+print(f'{a} * {b} * {c} = {a * b * c}')
+print(f'{a} : {b} : {c} = {a // b // c}')
