340e78dd855d7ccab2d07a6ebc29f5baa2c78b98
Andriy <andriyafanasenko66@gmail.com>
2024-03-03 14:14:02 +0200

    V.8

--- PythonSenior/1ClassWork.py
+++ PythonSenior/1ClassWork.py
@@ -2,3 +2,6 @@ print("Python")
 a = input("What your name: ")
 print(a + ", welcome to new lesson" + " - Python Senior")
 b = input("Do you complete lessons: Python Kids and Python Junior. Yes or No: ")
+if b == "NO" or "no" or "nO" or "Or":
+    print("You are not ready to this lesson")
+
