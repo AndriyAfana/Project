b5a37626f8d1dc95610251240ca30fc73ea2d9d0
Andriy <andriyafanasenko66@gmail.com>
2024-03-21 16:11:27 +0200

    V.1

---
+++ PythonSenior/4ClassWork/4ClassWork.py
@@ -0,0 +1,24 @@
+from Character import Character
+import time
+
+player1 = Character('cmetankaicalo', damage=50, health=100)
+player1.show_info()
+
+
+player2 = Character('caloiborch', damage=25, health=100)
+print(player2)
+
+while True:
+      time.sleep(2.000)
+      player1.is_alive()
+      player2.is_alive()
+      p1_damage = player1.attack (player2)
+      print(f'{player1.name} aтakував {player2.name} '
+            f'i нанic {p1_damage} шкоди.')
+      p2_damage = player2.attack (player1)
+      print(f'{player2.name} aтакував {player1.name} ' 
+            f'1 наніс {p2_damage} шкоди.')
+      print(player1, player2, sep='\n')
+
+
+
